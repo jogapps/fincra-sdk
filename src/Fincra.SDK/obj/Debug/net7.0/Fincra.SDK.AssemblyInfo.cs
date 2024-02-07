1b95fc1278d90bb30dc2f849260a9837d9d5a438
jogapps <jogappsdeveloper@gmail.com>
2024-02-07 09:13:49 +0100

    updated meta data company name

--- src/Fincra.SDK/obj/Debug/net7.0/Fincra.SDK.AssemblyInfo.cs
+++ src/Fincra.SDK/obj/Debug/net7.0/Fincra.SDK.AssemblyInfo.cs
@@ -10,7 +10,7 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Jemifor Godspower")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Fincra")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyDescriptionAttribute("Fincra Dotnet SDK")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
